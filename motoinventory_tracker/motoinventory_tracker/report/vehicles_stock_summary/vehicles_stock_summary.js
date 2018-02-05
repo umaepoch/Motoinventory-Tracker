4c9828c4fcb2a6286462dad83154c89b3f814312
Uma <umag@epochconsulting.in>
2018-02-05 12:25:06 +0530

    Allocation Problems

--- motoinventory_tracker/motoinventory_tracker/report/vehicles_stock_summary/vehicles_stock_summary.js
+++ motoinventory_tracker/motoinventory_tracker/report/vehicles_stock_summary/vehicles_stock_summary.js
@@ -2,7 +2,7 @@
 // For license information, please see license.txt
 /* eslint-disable */
 
-frappe.query_reports["Modified Stock Balance"] = {
+frappe.query_reports["Vehicles Stock Summary"] = {
 	"filters": [
 		{
 			"fieldname":"from_date",
