3e8ce614bc02a3b73aa1aa811bec7703407afbcd
Uma <umag@epochconsulting.in>
2018-05-02 13:11:41 +0530

    DSR

---
+++ motoinventory_tracker/motoinventory_tracker/report/daily_stock_report___summary/daily_stock_report___summary.js
@@ -0,0 +1,15 @@
+// Copyright (c) 2016, Epoch and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Daily Stock Report - Summary"] = {
+	"filters": [
+		{
+			"fieldname": "warehouse",
+			"label": __("Warehouse"),
+			"fieldtype": "Link",
+			"width": "80",
+			"options": "Warehouse"
+		}
+	]
+}
