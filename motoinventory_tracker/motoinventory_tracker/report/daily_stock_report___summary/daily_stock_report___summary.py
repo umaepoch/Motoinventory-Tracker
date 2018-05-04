fee1823669dd12c99b1423bae60ee2e339085ad2
Uma <umag@epochconsulting.in>
2018-05-04 09:12:16 +0530

    DSR

--- motoinventory_tracker/motoinventory_tracker/report/daily_stock_report___summary/daily_stock_report___summary.py
+++ motoinventory_tracker/motoinventory_tracker/report/daily_stock_report___summary/daily_stock_report___summary.py
@@ -101,9 +101,7 @@ def execute(filters=None):
 				unalloc_whse_count = 1
 				
 		total_count = total_count +1
-	if whse_count == 1:
-#		data.append([whse_work, item_work, serial_work, vehstatus_work, brn_work, ""])	
-#		data.append(["", item_work, "", "", "", item_count])
+
 	tot_alloc_whse_count = tot_alloc_whse_count + alloc_whse_count
 	tot_unalloc_whse_count = tot_unalloc_whse_count + unalloc_whse_count
 
